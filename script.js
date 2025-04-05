33a8c51e3b51ca7d37d204e00fcbd80ba00bbdc3
vikrantsrgc <145971065+vikrantsrgc@users.noreply.github.com>
2025-04-05 12:02:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,25 @@
 // Complete the js code
-function Car(make, model) {}
+class Car{
+	constructor(make,model){
+		this.make = make 
+		this.model = model
+	}
+	getMakeModel(){
+		return `${this.make} ${this.model}` 
+	}
+}
 
-function SportsCar(make, model, topSpeed) {}
+class SportsCar extends Car {
+	constructor(make,model,topSpeed){
+		super(make,model) // call the parent constructor
+		this.topSpeed = topSpeed
+	}
+
+	getTopSpeed(){
+		return this.topSpeed
+	}
+	
+}
 
 // Do not change the code below
 window.Car = Car;
